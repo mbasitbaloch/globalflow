f78a78be4cba53e763fc3c9e9a3819754d30b34f
mbasitbaloch <mabdulnutkani786@gmail.com>
2025-12-19 13:14:11 +0500

    updated qdrant vector

--- app/routes/translate.py
+++ app/routes/translate.py
@@ -1258,7 +1258,7 @@ async def update_translated_string(req: UpdateRequest, db: Session = Depends(get
             embedding = response.data[0].embedding
             correction_point = PointStruct(
                 id=str(uuid.uuid4()),
-                vector={"globalflow": embedding},
+                vector=embedding,
                 payload={
                     "data_type": "correction",
                     "user_id": str(user["_id"]) if user else None,
@@ -1531,7 +1531,7 @@ async def update_translated_string(req: UpdateRequest, db: Session = Depends(get
 #             embedding = response.data[0].embedding
 #             correction_point = PointStruct(
 #                 id=str(uuid.uuid4()),
-#                 vector={"GlobalFlow": embedding},
+#                 vector={"default": embedding},
 #                 payload={
 #                     "data_type": "correction",
 #                     "user_id": str(user["_id"]) if user else None,
